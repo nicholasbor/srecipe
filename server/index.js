a680d92d67ba163c6d939bacd4c6da42e02720e1
Nicholas Bor <nicholas.bor27@gmail.com>
2022-01-26 18:02:55 +1100

    base files and database connection

---
+++ server/index.js
@@ -0,0 +1,13 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(cors());
+
+app.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}`);
+})
